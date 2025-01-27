5758d99e33fa611a0557c777a6883f46715c575b
Ph0qu3_111 <79283690+BananeRapeuse@users.noreply.github.com>
2025-01-27 07:52:56 +0100

    Update XIS.h

--- XIS.h
+++ XIS.h
@@ -1,5 +1,24 @@
 #pragma once
 
-void ToggleXIS(bool enable);
+#include <d3d11.h>
+#include <wrl.h>
+#include <string>
 
-extern bool g_xisEnabled;
+class XIS {
+public:
+    bool Initialize(ID3D11Device* device, ID3D11DeviceContext* context, int width, int height);
+    void Render(ID3D11DeviceContext* context, ID3D11ShaderResourceView* sceneSRV);
+    void Cleanup();
+
+private:
+    Microsoft::WRL::ComPtr<ID3D11Texture2D> m_IntermediateTexture;
+    Microsoft::WRL::ComPtr<ID3D11RenderTargetView> m_IntermediateRTV;
+    Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> m_IntermediateSRV;
+    Microsoft::WRL::ComPtr<ID3D11SamplerState> m_SamplerState;
+
+    Microsoft::WRL::ComPtr<ID3D11VertexShader> m_FullscreenQuadVS;
+    Microsoft::WRL::ComPtr<ID3D11PixelShader> m_BicubicUpsamplePS;
+
+    void CreateIntermediateRenderTarget(ID3D11Device* device, int width, int height);
+    void LoadShaders(ID3D11Device* device);
+};
